b6d627f7bfef51c83fee9ef9ca675227324f0cdf
AnanthChristy <ananthchristy@Ananths-MacBook-Air.local>
2018-03-16 11:32:28 -0700

    This is the server repo.

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+var app = express();
+app.use(express.static('public'));
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('home.html',{root: 'public'});
+});
+
+module.exports = router;
